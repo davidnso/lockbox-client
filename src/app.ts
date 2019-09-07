6eefed314d0e4aaaa2a1cea84caec6307c528dc2
davidnso <davidnsoesie1@gmail.com>
2019-09-06 23:06:12 -0400

    :construction: add dotenv config

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import { ExpressDriver } from "./drivers/express/expressDriver";
 import * as http from "http";
-
+require("dotenv").config();
 const app = ExpressDriver.build();
 /**
  * build the express driver and start the application server.
  */
 const server = http.createServer(app);
-server.listen(4000, () => {
-  console.log("LockBox Api running on port 4000");
+server.listen(process.env.PORT, () => {
+  console.log("LockBox Api running on port " + process.env.PORT);
 });
