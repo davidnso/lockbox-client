823cb3de3d9a1e97a18fd5cf228a00ce6e8735d1
davidnso <davidnsoesie1@gmail.com>
2019-09-06 21:10:12 -0400

    :construction: add gulp file and necessary dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,35 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+var nodemon = require("gulp-nodemon");
 
 var tsProject = ts.createProject("tsconfig.json");
+const jsonDocs = ["package.json"];
+
+const runDevelopmentServer = function() {
+  return nodemon({
+    script: "dist/app.js",
+    ext: "js"
+  });
+};
 
 gulp.task("typescript", function() {
   return tsProject
     .src()
-    .pipe(ts(tsProject))
+    .pipe(tsProject())
     .pipe(gulp.dest("dist"));
 });
 
 gulp.task("watch", function() {
-  gulp.watch("src/*.ts", gulp.series("typescript"));
+  gulp.watch("src/**/*.ts", gulp.series("typescript"));
 });
 
-gulp.task("default", gulp.series("watch"));
+gulp.task("assets", function() {
+  return gulp.src(jsonDocs).pipe(gulp.dest("dist"));
+});
+
+gulp.task(
+  "start",
+  gulp.series("tsc", gulp.parallel("watch", runDevelopmentServer))
+);
+
+gulp.task("default", gulp.series("typescript"), "assets");
