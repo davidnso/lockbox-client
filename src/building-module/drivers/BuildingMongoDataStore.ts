27afc72f34d6f5091c0d9e3515f750237ff0f2f0
davidnso <davidnsoesie1@gmail.com>
2019-10-01 18:42:09 -0400

    : construction: add  building handler and datastore

---
+++ src/building-module/drivers/BuildingMongoDataStore.ts
@@ -0,0 +1,65 @@
+import { MongoDriver } from "../../drivers";
+import { throws } from "assert";
+
+export class BuildingMongoDataStore {
+  buildingStore: any;
+  constructor() {
+    MongoDriver.buildDriver(process.env.DB_URI as string, "buildings").then(
+      store => {
+        this.buildingStore = store;
+      }
+    );
+  }
+
+  async findAllBuildings() {
+    this.buildingStore.find({});
+  }
+
+  async findBuildingsByLUID({ luid }: { luid: string }) {
+    try {
+      const buildings = await this.buildingStore
+        .find({ buildingId: luid })
+        .toArray();
+      return buildings;
+    } catch (err) {
+      console.log("Mongo Error: ", err);
+    }
+  }
+
+  async fetchLogsbyBuildingId({ luid }: { luid: string }) {
+    try {
+      const accessLogs = await this.buildingStore
+        .aggregate([
+          {
+            $match: {
+              buildingId: luid
+            }
+          },
+          {
+            $lookup: {
+              from: "access-logs",
+              localField: "buildingId",
+              foreignField: "buid",
+              as: "accessLogs"
+            }
+          },
+          {
+            $unwind: {
+              path: "$accessLogs",
+              includeArrayIndex: false,
+              preserveNullAndEmptyArrays: true
+            }
+          },
+          {
+            $replaceRoot: {
+              newRoot: "$accessLogss"
+            }
+          }
+        ])
+        .toArray();
+      return accessLogs[0];
+    } catch (err) {
+      console.log("Mongo Error: ", err);
+    }
+  }
+}
