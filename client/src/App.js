659f0ed059b468498929218c8698933be58a8804
davidnso <davidnsoesie1@gmail.com>
2019-10-04 20:35:46 -0400

    :construction: init react client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <div>
+        <p>It's time to begin...</p>
+      </div>
+    </body>
+  );
+}
+
+export default App;
