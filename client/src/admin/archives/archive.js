d3fc356e1a5e1a0989b7355479c0f1a519671aca
davidnso <davidnsoesie1@gmail.com>
2019-11-06 20:24:12 -0500

    table view work in progress

--- client/src/admin/archives/archive.js
+++ client/src/admin/archives/archive.js
@@ -6,6 +6,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Axios from "axios";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -17,8 +18,11 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(id, name, building, date,time, status) {
+  if(!status){
+    status='granted';
+  }
+  return { id, name, building, date,time, status };
 }
 
 const rows = [
@@ -29,8 +33,36 @@ const rows = [
   createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
+
+
 export default class Archive extends Component {
    classes = useStyles;
+  state={
+    tableData: []
+  }
+componentWillMount(){
+  this.fetchOnLoad().then(()=>{})
+}
+
+async fetchOnLoad(){
+  let logRow = [];
+  Axios.get(`http://localhost:4100/logs`).then( apiResponse=>{
+    const logs = apiResponse.data;
+    
+    logs.map(async log=>{
+    const apiResponse = await Axios.get(`http://localhost:4100/buildings/${log.buildingId}`)
+    delete log.buildingId;
+    log.buildingName = apiResponse.data[0].name;
+    const status = 'Accepted';
+    const date = new Date(Number.parseInt(log.date));
+    const time = date.toLocaleTimeString();
+    const day = date.getDate();
+      logRow.push(createData(log._id,log.username,log.buildingName,day, time,status));
+    })
+    console.log(logRow)
+  })
+  this.setState({tableData: logRow});
+}
 
   render() {
     return (
@@ -52,25 +84,28 @@ export default class Archive extends Component {
     <Table className={this.classes.table}>
       <TableHead>
         <TableRow>
-          <TableCell>Dessert (100g serving)</TableCell>
-          <TableCell align="right">Calories</TableCell>
-          <TableCell align="right">Fat&nbsp;(g)</TableCell>
-          <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-          <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          <TableCell>ID</TableCell>
+          <TableCell align="right">Name</TableCell>
+          <TableCell align="right">Building&nbsp;</TableCell>
+          <TableCell align="right">Carbs&nbsp;</TableCell>
+          <TableCell align="right">Date&nbsp;</TableCell>
+          <TableCell align="right">Time&nbsp;</TableCell>
+          <TableCell align="right">Status&nbsp;</TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
-        {rows.map(row => (
-          <TableRow key={row.name}>
+        {this.state.tableData? this.state.tableData.map(row => (
+          <TableRow key={row.id}>
             <TableCell component="th" scope="row">
-              {row.name}
+              {row.id}
             </TableCell>
-            <TableCell align="right">{row.calories}</TableCell>
-            <TableCell align="right">{row.fat}</TableCell>
-            <TableCell align="right">{row.carbs}</TableCell>
-            <TableCell align="right">{row.protein}</TableCell>
+            <TableCell align="right">{row.name}</TableCell>
+            <TableCell align="right">{row.buildingName}</TableCell>
+            <TableCell align="right">{row.day}</TableCell>
+            <TableCell align="right">{row.time}</TableCell>
+            <TableCell align="right">{row.status}</TableCell>
           </TableRow>
-        ))}
+        )): null}
       </TableBody>
     </Table>
   </Paper>
