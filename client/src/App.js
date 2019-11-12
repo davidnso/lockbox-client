a0ee24927d388c28939140efece36d089e52fc92
davidnso <davidnsoesie1@gmail.com>
2019-11-12 15:04:30 -0500

    :construction: add base page for routing to proper user groups

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Navigation from "./navigation/navigation";
-require('typeface-nunito')
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+require("typeface-nunito");
 
-function App() {
-  return (
-      <Navigation />
-  );
+function renderNavigation() {
+  return <Navigation />;
+}
+
+export class App extends Component {
+  state = {
+    isVisible: true
+  };
+  render() {
+    return (
+      <Router>
+        {this.state.isVisible && (
+          <div className="context" >
+            <Link
+              className="button"
+              style={{ textDecoration: "none" }}
+              onClick={() => this.setState({ isVisible: false })}
+              to="/student"
+            >
+              <p>Student</p>
+            </Link>
+
+            <Link
+              className="button"
+              style={{ textDecoration: "none" }}
+              onClick={() => this.setState({ isVisible: false })}
+              to="/admin"
+            >
+              <p>Administrator</p>
+            </Link>
+          </div>
+        )}
+        <Switch>
+          <Route path="/student">
+            <Navigation context="student" />
+          </Route>
+          <Route path="/admin">
+            <Navigation context="administrator" />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- client/src/navigation/navigation.js
+++ client/src/navigation/navigation.js
@@ -7,26 +7,99 @@ import {
   Redirect
 } from "react-router-dom";
 import AdminDashboard from "../dashboard/admin/dashboard";
-import StudentDashboard from '../dashboard/student/dashboard'
+import StudentDashboard from "../dashboard/student/dashboard";
 import Accounts from "../admin/accounts/accounts";
 import Tasks from "../admin/tasks/tasks";
 import Archive from "../admin/archives/archive";
 import Monitoring from "../admin/monitoring/monitoring";
 import "./navigation.css";
-import {logo} from '../resources/lock-logo.png'
+import { logo } from "../resources/lock-logo.png";
 import Tickets from "../students/tickets/tickets";
 export default class Navigation extends Component {
-  constructor(props){
-    super(props)
-    this.state= {
+  constructor(props) {
+    super(props);
+    this.state = {
       student: true,
-      admin: false,
-    }
+      admin: false
+    };
   }
   render() {
     return (
+      <>
       <Router>
-        <div className="container">
+        {this.props.context == "administrator" && (
+          <>
+            <div className="container">
+              <div className="logo" src={logo}></div>
+              <hr />
+              <nav>
+                <ul>
+                  <li>
+                    <Link style={{ textDecoration: "none" }} to="/home">
+                      Dashboard
+                    </Link>
+                  </li>
+                  <li>
+                    <Link style={{ textDecoration: "none" }} to="/monitoring">
+                      Monitoring
+                    </Link>
+                  </li>
+                  <li>
+                    <Link style={{ textDecoration: "none" }} to="/accounts">
+                      Accounts
+                    </Link>
+                  </li>
+                  <li>
+                    <Link style={{ textDecoration: "none" }} to="/tasks">
+                      Task Master
+                    </Link>
+                  </li>
+                  <li>
+                    <Link style={{ textDecoration: "none" }} to="/archive">
+                      Archive
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+              <img
+                src={require("../resources/bottom-image.png")}
+                style={{
+                  position: "absolute",
+                  left: 0,
+                  bottom: 0,
+                  width: "160px",
+                  height: "160x"
+                }}
+              ></img>
+            </div>
+
+            <Switch>
+              <Route
+                exact
+                path="/admin"
+                render={() => <Redirect to="/home" />}
+              ></Route>
+              <Route path="/home">
+                <AdminDashboard />
+              </Route>
+              <Route path="/monitoring">
+                <Monitoring />
+              </Route>
+              <Route path="/accounts">
+                <Accounts />
+              </Route>
+              <Route path="/tasks">
+                <Tasks />
+              </Route>
+              <Route path="/archive">
+                <Archive />
+              </Route>
+            </Switch>
+          </>
+        )}
+      </Router>
+        <Router>
+{       this.props.context == 'student' && <><div className="container">
           <div className='logo' src={logo}></div>
           <hr/>
           <nav>
@@ -35,16 +108,10 @@ export default class Navigation extends Component {
                 <Link style={{textDecoration:"none"}} to="/home">Dashboard</Link>
               </li>
               <li>
-                <Link style={{textDecoration:"none"}} to="/monitoring">Monitoring</Link>
+                <Link style={{textDecoration:"none"}} to="/tickets">Tickets</Link>
               </li>
               <li>
-                <Link style={{textDecoration:"none"}} to="/accounts">Accounts</Link>
-              </li>
-              <li>
-                <Link style={{textDecoration:"none"}} to="/tasks">Task Master</Link>
-              </li>
-              <li>
-                <Link style={{textDecoration:"none"}} to="/archive">Archive</Link>
+                <Link style={{textDecoration:"none"}} to="/notifications">notifications</Link>
               </li>
             </ul>
           </nav>
@@ -52,58 +119,21 @@ export default class Navigation extends Component {
         </div>
 
         <Switch>
-          <Route exact path="/" render={() => <Redirect to="/home" />}></Route>
+          <Route exact path="/student" render={() => <Redirect to="/home" />}></Route>
           <Route path="/home">
-            <AdminDashboard />
+            <StudentDashboard />
           </Route>
-          <Route path="/monitoring">
-            <Monitoring />
+          <Route path="/tickets">
+            <Tickets />
           </Route>
-          <Route path="/accounts">
+          <Route path="/notifications">
             <Accounts />
           </Route>
-          <Route path="/tasks">
-            <Tasks />
-          </Route>
-          <Route path="/archive">
-            <Archive />
-          </Route>
-        </Switch>
-      </Router>
-    //   <Router>
-    //   <div className="container">
-    //     <div className='logo' src={logo}></div>
-    //     <hr/>
-    //     <nav>
-    //       <ul>
-    //         <li>
-    //           <Link style={{textDecoration:"none"}} to="/home">Dashboard</Link>
-    //         </li>
-    //         <li>
-    //           <Link style={{textDecoration:"none"}} to="/tickets">Tickets</Link>
-    //         </li>
-    //         <li>
-    //           <Link style={{textDecoration:"none"}} to="/notifications">notifications</Link>
-    //         </li>
-    //       </ul>
-    //     </nav>
-    //     <img src={require('../resources/bottom-image.png')} style={{position:"absolute",left:0,bottom:0,width:'160px',height:'160x'}}></img>
-    //   </div>
 
-    //   <Switch>
-    //     <Route exact path="/" render={() => <Redirect to="/home" />}></Route>
-    //     <Route path="/home">
-    //       <StudentDashboard />
-    //     </Route>
-    //     <Route path="/tickets">
-    //       <Tickets />
-    //     </Route>
-    //     <Route path="/notifications">
-    //       <Accounts />
-    //     </Route>
-        
-    //   </Switch>
-    // </Router>
+        </Switch>
+      </>}
+      </Router>
+      </>
     );
   }
 }
